0183dc14467917df77bc065c72e0dcb462ff2942
Sachin Ninganure <sninganu@sninganu.pnq.csb>
2023-07-28 19:55:03 +0530

    modified run.sh for packet clusters

--- workloads/network-perf-v2/run.sh
+++ workloads/network-perf-v2/run.sh
@@ -34,7 +34,7 @@ echo "============ Debug Info ============"
 echo k8s-netperf version $NETPERF_VERSION
 oc get pods -n netperf -o wide
 oc get nodes -o wide
-oc get machineset -A
+#oc get machineset -A
 
 log "Finished workload ${0} ${WORKLOAD}, exit code ($run)"
 
