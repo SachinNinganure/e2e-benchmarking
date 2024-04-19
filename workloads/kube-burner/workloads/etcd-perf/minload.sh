998865f1fdeb9eae1205e18feabb9a4350152243
Sachin Ninganure <sninganu@sninganu.pnq.csb>
2024-04-19 13:07:18 +0530

    check-test

--- workloads/kube-burner/workloads/etcd-perf/minload.sh
+++ workloads/kube-burner/workloads/etcd-perf/minload.sh
@@ -1,22 +1,20 @@
  #CASE 01 create 100 projects in the batches of 500
-  for i in {1..10}; do oc new-project project-$i;oc -n project-$i create configmap project-$i --from-file=/etc/pki/ca-trust/source/anchors;done
+  #for i in {1..100}; do oc new-project project-$i;oc create configmap project-$i --from-file=/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt; done
+  for i in {1..2000}; do oc new-project project-$i;oc -n project-$i create configmap project-$i --from-file=/etc/pki/ca-trust/source/anchors;done
   date;oc adm top node
   echo "to check endpoint health after creating many projects"
   for i in ` oc -n openshift-etcd get pods | grep etcd-ip |awk '{print $1}'`; do oc -n openshift-etcd exec $i -- etcdctl endpoint health; done
-    
   #CASE 02 Many images
-  if ! oc get ns |grep multi-image >/dev/null;
+   if ! oc get ns |grep multi-image >/dev/null;
     then
       oc create ns multi-image;
-  fi
-  
-  for i in {1..10}; do oc -n multi-image process -f workloads/etcd-perf/template_image.yaml -p NAME=testImage-$i | oc -n multi-image create -f - ; done
+   fi
+  for i in {1..120000}; do oc -n multi-image process -f workloads/etcd-perf/template_image.yaml -p NAME=testImage-$i | oc -n multi-image create -f - ; done
   echo "to check endpoint health after creating many images"
   for i in ` oc -n openshift-etcd get pods | grep etcd-ip |awk '{print $1}'`; do oc -n openshift-etcd exec $i -- etcdctl endpoint health; done
-  
-  
   #CASE 03 Many secrets; 300namespaces each with 400 secrets
-  for i in {1..3}; do oc new-project sproject-$i; for j in {1..4}; do oc -n sproject-$i create secret generic my-secret-$j --from-literal=key1=supersecret --from-literal=key2=topsecret;done  done
+
+  for i in {1..300}; do oc new-project sproject-$i; for j in {1..400}; do oc -n sproject-$i create secret generic my-secret-$j --from-literal=key1=supersecret --from-literal=key2=topsecret;done  done
   #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   # Configure the name of the secret and namespace
   SECRET_NAME="my-large-secret"
@@ -37,37 +35,16 @@
   oc -n multi-image create -f workloads/etcd-perf/testsec.yaml
   rm -f sshkey sshkey.pub tls.crt tls.key
   git clone https://github.com/peterducai/etcd-tools.git;sleep 10;
-  
-#CASE 4 large secrets
-echo "Large secrets...!"
-for i in {3..10};
-do
-SECRET_NAME="my-large-secret-$i"
+  #To check the etcd pod load status
+  for i in {3..50000};
+   do
+    SECRET_NAME="my-large-secret-$i";oc -n multi-image create -f workloads/etcd-perf/my-large-sec.yaml
+   done
+  echo "to check endpoint health after creating many secrets"
 
-# Create the secret YAML file
-cat <<EOF | oc apply -f -
-apiVersion: v1
-kind: Secret
-metadata:
-  name: $SECRET_NAME
-  namespace: $NAMESPACE
-type: Opaque
-data:
-  ssh-private-key: $SSH_PRIVATE_KEY
-  ssh-public-key: $SSH_PUBLIC_KEY
-  token: $TOKEN_VALUE
-  tls.crt: $CERTIFICATE
-  tls.key: $PRIVATE_KEY
-EOF
-done
-# Clean up the generated files
-rm -f sshkey sshkey.pub tls.crt tls.key
-
-echo "to check endpoint health after creating many secrets"
-date;oc adm top node;date;etcd-tools/etcd-analyzer.sh;date
-
-
-echo "Fio Test STARTS...........................................................................!"
-#etcd-tools/fio_suite.sh
-etc_masternode1=`oc get node |grep master|awk '{print $1}'|tail -1`
-oc debug -n openshift-etcd --quiet=true node/$etc_masternode1 -- chroot host bash -c "podman run --privileged --volume /var/lib/etcd:/test quay.io/peterducai/openshift-etcd-suite:latest fio"
+  for i in ` oc -n openshift-etcd get pods | grep etcd-ip |awk '{print $1}'`; do oc -n openshift-etcd exec $i -- etcdctl endpoint health; done
+  date;oc adm top node;date;etcd-tools/etcd-analyzer.sh;date
+  #Fio Test STARTS...........................................................................!
+  #etcd-tools/fio_suite.sh
+  etc_masternode1=`oc get node |grep master|awk '{print $1}'|tail -1`
+  oc debug -n openshift-etcd --quiet=true node/$etc_masternode1 -- chroot host bash -c "podman run --privileged --volume /var/lib/etcd:/test quay.io/peterducai/openshift-etcd-suite:latest fio"
