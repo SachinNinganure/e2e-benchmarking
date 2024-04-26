d2ef3b372c3a76adb0ff7e419009dee002e4c327
Sachin Ninganure <sninganu@sninganu.pnq.csb>
2024-04-26 11:14:08 +0530

    25% load

--- workloads/kube-burner/workloads/etcd-perf/runetcdperf.sh
+++ workloads/kube-burner/workloads/etcd-perf/runetcdperf.sh
@@ -1,6 +1,6 @@
  #CASE 01 create 100 projects in the batches of 500
   #for i in {1..100}; do oc new-project project-$i;oc create configmap project-$i --from-file=/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt; done
-  for i in {1..1000}; do oc new-project project-$i;oc -n project-$i create configmap project-$i --from-file=/etc/pki/ca-trust/source/anchors;done
+  for i in {1..500}; do oc new-project project-$i;oc -n project-$i create configmap project-$i --from-file=/etc/pki/ca-trust/source/anchors;done
   date;oc adm top node
   echo "to check endpoint health after creating many projects"
   for i in ` oc -n openshift-etcd get pods | grep etcd-ip |awk '{print $1}'`; do oc -n openshift-etcd exec $i -- etcdctl endpoint health; done
@@ -9,12 +9,12 @@
     then
       oc create ns multi-image;
    fi
-  for i in {1..60000}; do oc -n multi-image process -f workloads/etcd-perf/template_image.yaml -p NAME=testImage-$i | oc -n multi-image create -f - ; done
+  for i in {1..30000}; do oc -n multi-image process -f workloads/etcd-perf/template_image.yaml -p NAME=testImage-$i | oc -n multi-image create -f - ; done
   echo "to check endpoint health after creating many images"
   for i in ` oc -n openshift-etcd get pods | grep etcd-ip |awk '{print $1}'`; do oc -n openshift-etcd exec $i -- etcdctl endpoint health; done
   #CASE 03 Many secrets; 300namespaces each with 400 secrets
 
-  for i in {1..100}; do oc new-project sproject-$i; for j in {1..200}; do oc -n sproject-$i create secret generic my-secret-$j --from-literal=key1=supersecret --from-literal=key2=topsecret;done  done
+  for i in {1..50}; do oc new-project sproject-$i; for j in {1..100}; do oc -n sproject-$i create secret generic my-secret-$j --from-literal=key1=supersecret --from-literal=key2=topsecret;done  done
   #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   # Configure the name of the secret and namespace
   SECRET_NAME="my-large-secret"
@@ -36,7 +36,7 @@
   rm -f sshkey sshkey.pub tls.crt tls.key
   git clone https://github.com/peterducai/etcd-tools.git;sleep 10;
   #To check the etcd pod load status
-  for i in {3..25000};
+  for i in {3..12500};
    do
     SECRET_NAME="my-large-secret-$i";oc -n multi-image create -f workloads/etcd-perf/my-large-sec.yaml
    done
